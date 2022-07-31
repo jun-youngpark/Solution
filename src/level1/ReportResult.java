a034403349590ca13080e2a54dae39a74604f35f
jyp <narajy@gmail.com>
2022-08-01 00:19:27 +0900

    신고결과받기

---
+++ src/level1/ReportResult.java
@@ -0,0 +1,51 @@
+package level1;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ReportResult {
+
+
+    //신고한사람 , 신고 받은 사람
+    public int[] solution(String[] id_list, String[] report, int k) {
+       // List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
+        //System.out.println(list);
+        HashMap<String,HashSet<String>> reporterInfoMap =new HashMap<String,HashSet<String>>();
+        HashMap<String,Integer> repotedCount =new HashMap<String,Integer>();
+        for (String reporter:id_list){
+            reporterInfoMap.put(reporter,new HashSet<>());
+            repotedCount.put(reporter,0);
+        }
+
+        for (String str:report){
+            String reporter= str.substring(0,str.indexOf(" "));
+            String reported = str.substring(str.indexOf(" ")+1);
+            if(reporterInfoMap.get(reporter).add(reported)) {   //존재하지않으면 true
+                repotedCount.put(reported, repotedCount.get(reported) + 1);
+            }
+        }
+
+        int[] answer = new int[id_list.length];
+        for(String repotered : repotedCount.keySet()){
+            if(repotedCount.get(repotered) >= k){
+                for (int i=0;i<id_list.length;i++) {
+                    if(reporterInfoMap.get(id_list[i]).contains(repotered)){
+                        answer[i] = answer[i] + 1;
+                    }
+                }
+
+            }
+        }
+        Arrays.stream(answer).forEach(System.out::println);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ReportResult rpt =new ReportResult();
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k=3;
+        rpt.solution(id_list,report,k);
+        //rpt.solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2);
+    }
+}
