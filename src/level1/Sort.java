b130907a3fe218f90545afc0ed03deb62a5e7d12
jyp <narajy@gmail.com>
2022-08-08 21:22:30 +0900

    Sort 사용하기

---
+++ src/level1/Sort.java
@@ -0,0 +1,28 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Sort {
+    public int[] solution(int[] array, int[][] commands) {
+
+        int[] answer = new int[commands.length];
+        for(int i=0;i<commands.length;i++){
+            int start = commands[i][0] -1;
+            int end = commands[i][1] ;
+            int postion = commands[i][2] -1;
+            int[] newArray =  Arrays.copyOfRange(array, start, end);
+            Arrays.sort(newArray);
+            Arrays.stream(newArray).forEach(System.out::println);
+            answer[i] = newArray[postion];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sort sort = new Sort();
+        int[] array ={1, 5, 2, 6, 3, 7, 4};
+        int[][] commands ={{2, 5, 3}};
+        sort.solution(array,commands);
+    }
+}
